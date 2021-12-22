884c8e0ee4ad0ba458fed062e6bafdaea5ed7ffc
Mina <minavu@Minas-MacBook-Pro.local>
2021-12-21 22:40:48 -0800

    Working on winning message. Game alg done.

--- match.js
+++ match.js
@@ -1,5 +1,4 @@
 let game_board = document.getElementById("game-board");
-let timer = 0;
 let WINNING_SCORE = 16;
 
 function changeGrid(size) {
@@ -7,11 +6,21 @@ function changeGrid(size) {
     WINNING_SCORE = size * size;
     game_board.classList.remove(...grids);
     game_board.classList.add(`grid-${size}x${size}`);
+    resetGame(size);
+}
+
+function resetGame(size) {
     document.querySelectorAll(".game-card").forEach(card => card.remove());
     document.getElementById("score").innerHTML = "000";
-    document.getElementById("time").innerHTML = "00:00";
+    document.getElementById("time").innerHTML = '<span id="minutes">00</span>:<span id="seconds">00</span>.<span id="tenths">00</span>';
     document.getElementById("moves").innerHTML = "000";
+    let pause_play = document.getElementById("pause-play");
+    pause_play.setAttribute("disabled", true);
+    pause_play.innerHTML = "PAUSE";
+    pause_play.classList.replace("play", "pause");
+    document.getElementById("reset").setAttribute("disabled", true);
     fetchCards(size);
+    stopWatch(size);
 }
 
 function fetchCards(size = 4) {
@@ -96,7 +105,7 @@ function checkMatch(all_flipped) {
         let score = document.getElementById("score").innerHTML;
         score = String(Number(score) + 2);
         document.getElementById("score").innerHTML = score.padStart(3, "0");
-        if (Number(score) === WINNING_SCORE) alert("YOU WIN!!!  CONGRATULATIONS!!!");
+        if (Number(score) === WINNING_SCORE) youWin();
     } else {
         all_flipped.forEach(card => {
             card.classList.remove("flipped-card");
@@ -105,12 +114,68 @@ function checkMatch(all_flipped) {
     }
 }
 
-// timer = setInterval(() => {
-//     let time = document.getElementById("time").innerHTML;
-//     time = time.split(":");
-//     String(Number(time[1])++).padStart(2, "0");
-//     time = time.join(":");
-//     console.log(time);
-// }, 1000);
+function youWin() {
+    alert("YOU WIN!!!  CONGRATULATIONS!!!");
+}
+
+function stopWatch(size = 4) {
+    let tens = 0;
+    let secs = 0;
+    let mins = 0;
+    let tenths = document.getElementById("tenths");
+    let seconds = document.getElementById("seconds");
+    let minutes = document.getElementById("minutes");
+    let pause_play = document.getElementById("pause-play");
+    let reset = document.getElementById("reset");
+    let interval = 0;
+
+    const watch = () => {
+        tens++;
+        if (tens > 99) {
+            secs++;
+            tens = 0;
+        }
+        if (secs > 59) {
+            mins++;
+            secs = 0;
+        }
+        tenths.innerHTML = String(tens).padStart(2, "0");
+        seconds.innerHTML = String(secs).padStart(2, "0");
+        minutes.innerHTML = String(mins).padStart(2, "0");
+    }
+
+    const start = () => {
+        interval = setInterval(watch, 10);
+        document.removeEventListener("click", start, true);
+        pause_play.removeAttribute("disabled");
+        reset.removeAttribute("disabled");
+    }
+
+    const stop = () => {
+        clearInterval(interval);
+    }
+
+    document.addEventListener("click", start, true);
+
+    pause_play.onclick = () => {
+        if (pause_play.classList.contains("pause")) {
+            stop();
+            pause_play.innerHTML = "PLAY";
+            pause_play.classList.replace("pause", "play");
+            document.querySelectorAll(".game-card-back").forEach(card => card.removeAttribute("onclick"));
+        } else {
+            start();
+            pause_play.innerHTML = "PAUSE";
+            pause_play.classList.replace("play", "pause");
+            document.querySelectorAll(".game-card-back").forEach(card => card.setAttribute("onclick", "flip(event);"));
+        }
+    }
+
+    reset.onclick = () => {
+        stop();
+        resetGame(size);
+    }
+}
 
 fetchCards();
+stopWatch();
